723fc7789127c542b9dfd5a72cd6b148e745bd81
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-07-20 18:42:25 -0700

    добавляет описание маршрутов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,12 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+import NotFound from '../../pages/not-found/not-found';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import PrivateRoute from '../private-route/private-route';
+import { HelmetProvider } from 'react-helmet-async';
 
 type AppScreenProps = {
   placesCount: number;
@@ -7,7 +14,25 @@ type AppScreenProps = {
 
 function App({placesCount}: AppScreenProps): JSX.Element {
   return (
-    <WelcomeScreen placesCount = {placesCount} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Main} element={<WelcomeScreen placesCount = {placesCount} />} />
+        </Routes>
+        <Routes>
+          <Route path={AppRoute.Login} element={<LoginScreen />} />
+        </Routes>
+        <Routes>
+          <Route path={AppRoute.Favotites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><FavoritesScreen /></PrivateRoute>} />
+        </Routes>
+        <Routes>
+          <Route path={AppRoute.Offer} element={<OfferScreen />} />
+        </Routes>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
