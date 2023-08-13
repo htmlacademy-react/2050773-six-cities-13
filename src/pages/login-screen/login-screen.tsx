fef3121945743fc573c06d313d14dca00eab5aa4
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-08-12 17:17:30 -0700

    добавляет FormEvent в LoginScreen

--- src/pages/login-screen/login-screen.tsx
+++ src/pages/login-screen/login-screen.tsx
@@ -1,22 +1,22 @@
 import { Helmet } from 'react-helmet-async';
-import { FormEvent, useRef } from 'react';
+import { FormEvent } from 'react';
 import { useAppDispatch } from '../../hooks/index';
 import { loginAction } from '../../store/api-actions';
+import { AuthData } from '../../types/auth-data';
 
 
 function LoginScreen(): JSX.Element {
-  const loginRef = useRef<HTMLInputElement | null>(null);
-  const passwordRef = useRef<HTMLInputElement | null>(null);
   const dispatch = useAppDispatch();
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
-    if (loginRef.current !== null && passwordRef.current !== null) {
-      dispatch(loginAction({
-        login: loginRef.current.value,
-        password: passwordRef.current.value,
-      }));
+    const form = evt.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData) as AuthData;
+
+    if (data !== null) {
+      dispatch(loginAction(data));
     }
   };
 
@@ -41,16 +41,37 @@ function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post" onSubmit={handleSubmit}>
+            <form
+              className="login__form form"
+              action="#"
+              method="post"
+              onSubmit={handleSubmit}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required ref={loginRef} />
+                <input
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required ref={passwordRef} />
+                <input
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
               </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
+              <button
+                className="login__submit form__submit button"
+                type="submit"
+              >Sign in
+              </button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
@@ -63,8 +84,6 @@ function LoginScreen(): JSX.Element {
         </div>
       </main>
     </div>
-
-
   );
 }
 
