2bbb69810ec2b44736b17120ddb293580b80c19a
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-08-29 15:36:18 -0700

    подключает компонент страницы без предложений

--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -7,6 +7,8 @@ import Sort from '../../components/sort/sort.tsx';
 import { sorting } from '../../utils.ts';
 import { useState } from 'react';
 import { getCity, getSortType } from '../../store/offers-process/offers-process.selector.ts';
+import MainEmpty from '../../components/main-empty/main-empty.tsx';
+import classNames from 'classnames';
 
 
 type WelcomeScreenProps = {
@@ -17,7 +19,7 @@ function WelcomeScreen({offers}: WelcomeScreenProps): JSX.Element {
 
   const city = useAppSelector(getCity);
   const sortType = useAppSelector(getSortType);
-  const currentOffers = sorting[sortType](offers.filter((offer) => offer.city.name === city));
+  const currentOffers = [];
   const [selectedOffer, setSelectedOffer] = useState<TOffer | undefined>(undefined);
 
   const handleCardHover = (point: TOffer) => {
@@ -26,30 +28,37 @@ function WelcomeScreen({offers}: WelcomeScreenProps): JSX.Element {
   };
 
   return (
-    <main className="page__main page__main--index">
+    <main className={`page__main page__main--index ${!currentOffers.length ? 'page__main--index-empty' : ''}`}>
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
           <CitiesList />
         </section>
       </div>
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{currentOffers.length} places to stay in {city}</b>
-            <Sort />
-            <div className="cities__places-list places__list tabs__content">
-              <CardList offers={currentOffers} onCardHover={handleCardHover}/>
+      {
+        currentOffers.length
+          ?
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{currentOffers.length} places to stay in {city}</b>
+                <Sort />
+                <div className="cities__places-list places__list tabs__content">
+                  <CardList offers={currentOffers} onCardHover={handleCardHover}/>
+                </div>
+              </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  <Map offers={currentOffers} selectedOffer={selectedOffer} />
+                </section>
+              </div>
             </div>
-          </section>
-          <div className="cities__right-section">
-            <section className="cities__map map">
-              <Map offers={currentOffers} selectedOffer={selectedOffer} />
-            </section>
           </div>
-        </div>
-      </div>
+          :
+          <MainEmpty city={city}/>
+      }
+
     </main>
   );
 }
